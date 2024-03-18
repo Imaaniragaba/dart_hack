2141b61da69e44e0d3e9bf082b21237f0e42720f
Martin Imaaniragaba <imaanir01@gmail.com>
2024-03-18 09:12:08 -0700

    Dart files

---
+++ functions.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+void main(){
+  int num1 = 9;
+  int num2 = 3;
+
+  num num_power = pow(num1 ,num2);
+  num square_root = sqrt(49);
+
+  print('$num1 to the power of $num2 is $num_power');
+  print('The squareroot of 49 is $square_root');
+}
---
+++ program.dart
@@ -0,0 +1,8 @@
+void main(){
+  String name = 'Martin';
+  int age = 27;
+  String school = 'PLP';
+  String hobby = 'Watching movies';
+
+  print('My name is $name, a $age year old student of $school and my hobby is $hobby.');
+}
